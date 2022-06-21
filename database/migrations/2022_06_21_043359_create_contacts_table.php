132097a773da4fb26f2d4c706dbccc2b8a3467ec
Abdul Motin <motin.mmk.jr@gmail.com>
2022-06-21 10:48:22 +0600

    laravel 9 faker

---
+++ database/migrations/2022_06_21_043359_create_contacts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('zip')->nullable();
+            
+            $table->unsignedBigInteger('company_id');
+            $table->timestamps();
+            //relationship between contact and company
+            $table->foreign('company_id')->references('id')->on('compamies')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
---
+++ database/seeders/ContactsTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class ContactsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // first get table and tranket is using DB 
+        $table = DB::table('contacts');
+        // then insert data
+        $contacts = [];
+        $faker = Faker::create();
+        foreach(range(1,100) as $index){
+            $contacts[] = [
+                'first_name' => $faker->firstName,
+                'last_name' => $faker->lastName,
+                'email' => $faker->email,
+                'phone' => $faker->phoneNumber,
+                'address' => $faker->address,
+                'city' => $faker->city,
+                'state' => $faker->state,
+                'zip' => $faker->postcode,
+                'company_id' => $faker->numberBetween(1,100),
+                'created_at' => now(),
+                'updated_at' => now()
+            ];
+        }
+        DB::table('contacts')->insert($contacts);
+        //
+    }
+}
